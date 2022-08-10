676a9c1a465b038de1e8a8e70a322274c595ea35
sabbat10 <sabbatlumpatshia99@gmail.com>
2022-08-10 17:39:26 +0100

    Amelioration de la partie avis

--- view/contact.php
+++ view/contact.php
@@ -1,23 +1,17 @@
-<?php
-$title = "Avis";
-ob_start();
+<?php 
+    $title = "Laisser un avis";
+    ob_start();
 
-require('header.php');
-?>
-
+    require('header.php');
+ ?>
 <div class="container">
     <div class="row">
-        <div class="col-lg-6 col-sm-8 ">
-            <h2>Vous pouvez nous laisser un message d'accourangement</h2>
-            <p class="encouragent">
-                C'est toujours un grand plaisir de récevoir toutes les accourangement venant de votre part nous vous
-                sommes toujours
-                réconnaissant; voir que vous êtes toujours à l'écoute de nos dernières nouvelles et de pouvoir en
-                contruibués nous donne les raison de continuer
-            </p>
+        <p class="text-center mt-5">Tous les avis des utilisateurs</p>
+        <div class="col-lg-4 col-sm-8 ">
+
             <form action="" method="post" class="text-start">
-                <div class="card contact bg-secondary p-2 ">
-                    <h3 class="text-decoration-underline text-light m-2 mb-3">*Laisser votre avis</h3>
+                <div class="card contact bg-secondary p-2 mt-5">
+                    <h5 class=" text-light m-2 mb-3">*Laissez nous votre avis</h5>
                     <p>
                         <input type="text" name="Nom" id="nom" placeholder="*Nom">
                     </p>
@@ -30,39 +24,43 @@ require('header.php');
                     </p>
 
                     <p>
-                        <textarea name="" id="Message" cols="50" rows="10"></textarea>
+                        <textarea name="" id="Message" cols="30" rows="5"></textarea>
                     </p>
-                    <button class="button btn-outline-success rounded-3" type="submit">Ajouter un avis</button>
+                    <button class="button btn-outline-success rounded-3" type="submit">Ajouter un
+                        avis</button>
                 </div>
             </form>
         </div>
 
         <!-- note de l'auteur -->
-        <div class="col-lg-6 col-sm-12">
-            <div class="card m-5" style="width: 20rem;">
-                <img src="../public/images/photos/istockphoto-891010944-170667a.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Note de l'auteur</h5>
-                    <p class="card-text">
-                        Bonjour, personne ne parfait ou parfaite da la vie.. Il y a toujours mieux
-                        que c'est que nous croyons détenir.
-                    </p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Encouuragement</li>
-                    <li class="list-group-item">Idées</li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link">A-propos de l'auteur</a>
-                    <a href="#" class="card-link">Voir le blog</a>
-                </div>
+        <div class=" avis col-6 col-ms-12 m-5">
+
+
+            <div class=" avis col-lg-8 col-ms-12 ms-5">
+                <p class="h5">Sabbat Lumpatshia</p>
+                <p>19 Aout 2022</p>
+                <p class="h5">4 / 5</p>
+                <p>C'est un site vraiment beau C'est un site vraiment beau C'est un site vraiment beau C'est un site
+                    vraiment beau</p>
+                <hr>
             </div>
+
+            <div class=" avis col-lg-8 col-ms-10 ms-5">
+                <p class="h5">Sabbat Lumpatshia</p>
+                <p>14 juin 2022</p>
+                <p class="h5">3 / 5</p>
+                <p>C'est un site vraiment beau C'est un site vraiment beau C'est un site vraiment beau C'est un site
+                    vraiment beau</p>
+                <hr>
+            </div>
+
         </div>
     </div>
 </div>
 
-<?php
-$content = ob_get_clean();
-require('base.php');
-require_once('footer.php');
-?>
+<?php 
+    $content = ob_get_clean();
+    require('base.php');
+
+    require('footer.php');
+ ?>
